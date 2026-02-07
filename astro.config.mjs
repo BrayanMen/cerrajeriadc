53022629f873cef4571b896daafae4f9b2aef0f0
brayanjmr880 <brayanjmr880@gmail.com>
2026-02-07 16:23:42 +0000

    refix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cerrajeriadc.com.ar',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
   },
